c021de82c11919d63a70742473326b3c21af866d
Gion Kunz <gion.kunz@gmail.com>
2016-11-22 22:59:21 +0100

    Getting more serious

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const rpio = require('rpio');
 
 const ledPin = 12;
-const ledClockDivider = 256;
-//const fanPin = 7;
+const ledClockDivider = 16;
 
 rpio.init({
   gpiomem: false
@@ -12,12 +11,9 @@ rpio.open(ledPin, rpio.PWM);
 rpio.pwmSetClockDivider(ledClockDivider);
 rpio.pwmSetRange(ledPin, 1024);
 
-//rpio.open(fanPin, rpio.OUTPUT, rpio.LOW);
-
 let counter = 0;
 const interval = setInterval(() => {
   rpio.pwmSetData(ledPin, (counter++ % 1024));
-  //rpio.write(fanPin, (counter % 50) > 25 ? rpio.HIGH : rpio.LOW);
   console.log('PWM: ' + counter % 1024);
 }, 30);
 
